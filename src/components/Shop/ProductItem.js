3e353f9d5bf687ccfbe7282f0997cb051041fe27
Kalin Hristov <kalin0169@gmail.com>
2023-12-21 11:59:37 +0200

    Connect Add To Cart Button to the cart-slice State!

--- src/components/Shop/ProductItem.js
+++ src/components/Shop/ProductItem.js
@@ -17,22 +17,22 @@ const ProductItem = (props) => {
         price: price,
       })
     );
-
-    return (
-      <li className={classes.item}>
-        <Card>
-          <header>
-            <h3>{title}</h3>
-            <div className={classes.price}>${price.toFixed(2)}</div>
-          </header>
-          <p>{description}</p>
-          <div className={classes.actions}>
-            <button onClick={addToCartHandler}>Add to Cart</button>
-          </div>
-        </Card>
-      </li>
-    );
   };
+
+  return (
+    <li className={classes.item}>
+      <Card>
+        <header>
+          <h3>{title}</h3>
+          <div className={classes.price}>${price.toFixed(2)}</div>
+        </header>
+        <p>{description}</p>
+        <div className={classes.actions}>
+          <button onClick={addToCartHandler}>Add to Cart</button>
+        </div>
+      </Card>
+    </li>
+  );
 };
 
 export default ProductItem;
