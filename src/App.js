abb6203bc606f957505e48da4ee33606dca45e42
Kalin Hristov <kalin0169@gmail.com>
2023-12-22 13:23:07 +0200

    App component has a subscription to the cart State and override the Data in the DataBase if there is such change.

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useEffect } from 'react';
 import { useSelector } from 'react-redux';
 
 import Cart from './components/Cart/Cart';
@@ -6,6 +7,15 @@ import Products from './components/Shop/Products';
 
 function App() {
   const showCart = useSelector((state) => state.ui.cartIsVisible);
+  const cart = useSelector((state) => state.cart);
+
+  // Override the existing cart data in the database with the latest cart data
+  useEffect(() => {
+    fetch('https://react-http-51b6e-default-rtdb.firebaseio.com/cart.json', {
+      method: 'PUT',
+      body: JSON.stringify(cart),
+    });
+  },[cart]);
 
   return (
     <Layout>
