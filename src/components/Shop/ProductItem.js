8387243ead345385e19b67ef4d71ce2166f08652
Kalin Hristov <kalin0169@gmail.com>
2023-12-22 12:03:42 +0200

    I make all the Logic of the cart-slice State in the ProductItem Component.
    I dispatch a new Action Object to the cart-slice reducers in the ProductItem Component so that I can later send the data on the Server. I can not do this in the Reducer Directly because it is not allowed to do a side effects on my Reducer!

--- src/components/Shop/ProductItem.js
+++ src/components/Shop/ProductItem.js
@@ -1,22 +1,53 @@
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
 import Card from '../UI/Card';
 import classes from './ProductItem.module.css';
 import { cartActions } from '../../store/cart-slice';
 
 const ProductItem = (props) => {
+  const cart = useSelector((state) => state.cart);
   const dispatch = useDispatch();
 
   const { id, title, price, description } = props;
 
   const addToCartHandler = () => {
-    dispatch(
-      cartActions.addItemToCart({
+    const newTotalQuantity = cart.totalQuantity + 1;
+
+    const updatedItems = cart.items.slice(); //create a copy via slice to avoid mutating the original state
+    const existingItem = updatedItems.find((item) => item.id === id);
+    if (existingItem) {
+      const updatedItem = { ...existingItem }; //create a copy via spread operator to avoid mutating the original state
+      updatedItem.quantity++;
+      updatedItem.totalPrice = updatedItem.totalPrice + price;
+      const existingItemIndex = updatedItems.findIndex(
+        (item) => item.id === id
+      );
+      updatedItems[existingItemIndex] = updatedItem;
+    } else {
+      updatedItems.push({
         id: id,
-        title: title,
         price: price,
-      })
-    );
+        quantity: 1,
+        totalPrice: price,
+        name: title,
+      });
+    }
+
+    const newCart = {
+      totalQuantity: newTotalQuantity,
+      items: updatedItems,
+    };
+
+    dispatch(cartActions.replaceCart(newCart));
+    // dispatch(
+    //   cartActions.addItemToCart({
+    //     id: id,
+    //     title: title,
+    //     price: price,
+    //   })
+    // );
+
+    // and then send Http request
   };
 
   return (
